accbedbc362dd4467bd55f3cfcc70f0950be3d7c
anhtn512 <anhtn512@gmail.com>
2018-11-19 06:47:18 -0800

    upload code

---
+++ Shop_client/client.py
@@ -0,0 +1,19 @@
+import socket
+
+RECV_BUFFER = 4096
+HOST = "localhost"
+PORT = 4000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST,PORT))
+
+while True:
+    message = client.recv(RECV_BUFFER)
+    print(message.decode())
+    message = input('$$$: ')
+    if message:
+        client.send(message.encode())
+    else:
+        break
+client.close()
+
---
+++ Shop_server/server.py
@@ -0,0 +1,43 @@
+import socket
+from threading import *
+import sys
+
+RECV_BUFFER = 4096
+HOST = '0.0.0.0'
+PORT = 4000
+
+#config server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    server.bind((HOST, PORT))
+except:
+    print ('Bind fail. ')
+    sys.exit()
+server.listen(10)
+print('Server listening .....')
+list_client = []
+
+def clientThead(conn, addr):
+    conn.send(b"Welcome to shop atoz!")
+    while True:
+        try:
+            message = conn.recv(RECV_BUFFER)
+            if message:
+                print ("<" + addr[0] + "> " + message.decode())
+                message_to_send = "<" + addr[0] + "> " + message.decode()
+                conn.send(message_to_send.encode())
+            else:
+                conn.close()
+                list_client.remove(conn)
+        except:
+            print('ahihi')
+            continue
+
+while True:
+    conn, addr = server.accept()
+    list_client.append(conn)
+    print(addr[0] + ' connected')
+    Thread(target=clientThead, args=(conn, addr,)).start()
+
+server.close()
